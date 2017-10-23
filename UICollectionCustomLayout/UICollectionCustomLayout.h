f4a4794f1b94bc666dc99fe3dbeb2a49e86ebdea
Yuma Matsune <yuma.matsune@gmail.com>
2017-10-23 21:58:13 +0900

    Initial Commit

---
+++ UICollectionCustomLayout/UICollectionCustomLayout.h
@@ -0,0 +1,19 @@
+//
+//  UICollectionCustomLayout.h
+//  UICollectionCustomLayout
+//
+//  Created by Yuma Matsune on 2017/10/23.
+//  Copyright © 2017年 matsune. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UICollectionCustomLayout.
+FOUNDATION_EXPORT double UICollectionCustomLayoutVersionNumber;
+
+//! Project version string for UICollectionCustomLayout.
+FOUNDATION_EXPORT const unsigned char UICollectionCustomLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UICollectionCustomLayout/PublicHeader.h>
+
+
